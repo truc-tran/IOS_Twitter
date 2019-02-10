db19ccf7493b81f789c7d678bbb9e52b104015b3
Truc Tran <tructran@Trucs-MacBook-Pro.local>
2019-02-10 00:38:07 -0800

    Twitter part 2 (add tweet and favorite heart)

---
+++ Twitter/TweetViewController.swift
@@ -0,0 +1,42 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by Truc Tran on 2/9/19.
+//  Copyright © 2019 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+
+    @IBOutlet weak var tweetTextView: UITextView!
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func tweet(_ sender: Any) {
+        //If the the Tweet text view is empty
+        if(!tweetTextView.text.isEmpty){
+            TwitterAPICaller.client?.postTweet(tweetString: tweetTextView.text, success: {
+                self.dismiss(animated: true, completion: nil)
+            }, failure: { (error) in
+                print("Error tweeeint post \(error)")
+                self.dismiss(animated: true, completion: nil)
+            })
+        } else {
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tweetTextView.becomeFirstResponder()
+    }
+    
+
+}
